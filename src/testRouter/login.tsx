e3cb1d3710c73d5e886869707d2c800a51c097d7
linjianhui <linjianhui@bixin.cn>
2023-05-14 22:05:26 +0800

    引入antd5，并且完成登陆页面

--- src/testRouter/login.tsx
+++ src/testRouter/login.tsx
@@ -1,9 +1,55 @@
-import React from "react"
+import React from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
 
-function Login() {
-  return (
-    <div>login</div>
-  )
-}
+// 登陆成功
+const onFinish = (values: any) => {
+  console.log('登陆 success:', values);
+  // 跳转路径
 
-export default Login
+};
+
+// 登陆失败
+const onFinishFailed = (errorInfo: any) => {
+  console.log('登陆 Failed:', errorInfo);
+};
+
+const Login: React.FC = () => (
+  <Form
+    name="basic"
+    labelCol={{ span: 8 }}
+    wrapperCol={{ span: 16 }}
+    style={{ maxWidth: 600 }}
+    initialValues={{ remember: true }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item
+      label="Username"
+      name="username"
+      rules={[{ required: true, message: '随意输入即可' }]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[{ required: true, message: '随意输入即可' }]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item>
+
+    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+);
+
+export default Login;
